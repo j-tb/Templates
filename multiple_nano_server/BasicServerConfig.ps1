ea029a3e2c3ac21b4458cc92f8c63acae5b3ecfb
Jennelle Crothers <jkcrothers@hotmail.com>
2016-10-13 14:38:37 -0700

    ok

---
+++ multiple_nano_server/BasicServerConfig.ps1
@@ -0,0 +1,24 @@
+function Disable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+    Stop-Process -Name Explorer
+    # Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+
+function Enable-IEDownloads {
+    $UserKey = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+    Set-ItemProperty -Path $UserKey -Name "1803" -Value 0
+    # Write-Host "IE downloads have been enabled." -ForegroundColor Green
+}
+
+# Run the functions to disable IE Enhanced Security and enable IE downloads
+Enable-IEDownloads
+Disable-InternetExplorerESC
+
+# Install AD Administration Tools locally
+Add-WindowsFeature RSAT-AD-PowerShell,RSAT-AD-AdminCenter
+
+
+
